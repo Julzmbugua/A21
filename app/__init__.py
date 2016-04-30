72bbc987727e5950dbc7791090bb5237decc49a4
Julius Mbugua <julzmbugua@gmail.com>
2016-04-30 23:13:35 +0300

    Login, logout, User registration, database use...

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
 
 
 login_manager = LoginManager()
@@ -17,6 +21,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    db.init_app(app)
    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
